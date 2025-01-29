7d42230cf9198eeeb3b0e1d0f025ad0a63f43c74
Umid Olimzhanov <uolimzhanov@gmail.com>
2025-01-29 17:05:32 +0500

    app: add NavigationSuiteScaffoldLayout and show screen in it

--- composeApp/src/commonMain/kotlin/com/uolimzhanov/bttrade/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/uolimzhanov/bttrade/app/App.kt
@@ -1,11 +1,51 @@
 package com.uolimzhanov.bttrade.app
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldLayout
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.uolimzhanov.bttrade.cashRegister.presentation.CashRegisterScreen
+import com.uolimzhanov.bttrade.core.presentation.lightGrayishBlue
+import com.uolimzhanov.bttrade.di.allModules
+import com.uolimzhanov.bttrade.shop.presentation.ShopTopAppBar
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.KoinApplication
 
 @Composable
 @Preview
-fun App() {
-    MaterialTheme {}
-}
+fun App() = KoinApplication(
+    application = { modules(allModules) },
+    content = {
+        MaterialTheme {
+            Surface(
+                modifier = Modifier
+                    .statusBarsPadding()
+                    .navigationBarsPadding()
+                    .fillMaxSize(),
+                color = lightGrayishBlue
+            ) {
+                NavigationSuiteScaffoldLayout(
+                    navigationSuite = {
+                        AppNavigationRail()
+                    }
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        ShopTopAppBar()
+                        CashRegisterScreen()
+                    }
+                }
+            }
+        }
+    }
+)
