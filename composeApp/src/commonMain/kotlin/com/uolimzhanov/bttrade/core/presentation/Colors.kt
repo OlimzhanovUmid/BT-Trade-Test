f2deaed1f0f4c87ee709b27e6055103c914501d1
Umid Olimzhanov <uolimzhanov@gmail.com>
2025-01-29 16:59:27 +0500

    core: add colors and shapes

---
+++ composeApp/src/commonMain/kotlin/com/uolimzhanov/bttrade/core/presentation/Colors.kt
@@ -0,0 +1,9 @@
+package com.uolimzhanov.bttrade.core.presentation
+
+import androidx.compose.ui.graphics.Color
+
+val coolLightGray = Color(0xfff2f5f7)
+val lightGreen = Color(0xff7bc78f)
+val lightGrayishBlue = Color(0xfff0f3f5)
+val classicBlue = Color(0xff2196f3)
+val scarletRed = Color(0xfffa5a5a)
---
+++ composeApp/src/commonMain/kotlin/com/uolimzhanov/bttrade/core/presentation/Shapes.kt
@@ -0,0 +1,8 @@
+package com.uolimzhanov.bttrade.core.presentation
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+val slightlyRoundedCornerShape = RoundedCornerShape(12.dp)
+val moderatelyRoundedCornerShape = RoundedCornerShape(16.dp)
+val highlyRoundedCornerShape = RoundedCornerShape(20.dp)
