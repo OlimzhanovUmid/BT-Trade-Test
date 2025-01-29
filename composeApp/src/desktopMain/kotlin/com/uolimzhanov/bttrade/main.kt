a0f62302ad96f78b1510a4fad24a9b209073bf9d
Umid Olimzhanov <uolimzhanov@gmail.com>
2025-01-29 16:52:06 +0500

    desktop: set Maximized state for window on start

--- composeApp/src/desktopMain/kotlin/com/uolimzhanov/bttrade/main.kt
+++ composeApp/src/desktopMain/kotlin/com/uolimzhanov/bttrade/main.kt
@@ -1,12 +1,17 @@
 package com.uolimzhanov.bttrade
 
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.uolimzhanov.bttrade.app.App
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
+        state = rememberWindowState(
+            placement = WindowPlacement.Maximized
+        ),
         title = "BT Trade Test",
     ) {
         App()
