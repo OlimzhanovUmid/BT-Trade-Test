d9ecdd67b2e5e1b84dc8667e34796d236c80bc00
Umid Olimzhanov <uolimzhanov@gmail.com>
2025-01-29 16:29:10 +0500

    gradle: add compose navigation

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -36,6 +36,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.compose.navigation)
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
